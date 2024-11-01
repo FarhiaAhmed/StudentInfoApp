447633b833951d07d1cfe229190ee966c422eb8f
FarhiaAhmed <fbuubiye@gmail.com>
2024-11-01 15:48:23 +0300

    commit10
    updating studentViewModel.java

--- app/src/main/java/com/example/studentinfoapp/StudentViewModel.java
+++ app/src/main/java/com/example/studentinfoapp/StudentViewModel.java
@@ -1,4 +1,27 @@
 package com.example.studentinfoapp;
 
-public class StudentViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentViewModel extends ViewModel {
+    private final MutableLiveData<List<StudentInfo>> studentInfoList = new MutableLiveData<>(new ArrayList<>());
+
+    public LiveData<List<StudentInfo>> getStudentInfoList() {
+        return studentInfoList;
+    }
+
+    public void addStudentInfo(StudentInfo info) {
+        List<StudentInfo> currentList = studentInfoList.getValue();
+        if (currentList != null) {
+            currentList.add(info);
+            studentInfoList.setValue(currentList);
+        }
+    }
+
+    public void clearStudentInfoList() {
+        studentInfoList.setValue(new ArrayList<>());
+    }
 }
